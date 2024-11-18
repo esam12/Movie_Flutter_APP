2b8fc085b4ad9f21b9d12e8d58f8f7bf08b34bca
ISAM ELZOBI <mobarmeg.9@gmail.com>
2024-11-18 23:59:44 +0300

    Add LoginBloc, LoginEvent, and LoginState for handling user login
    Implemented the LoginBloc to manage the login process, including handling login initiation, success, and error states. Added corresponding LoginEvent and LoginState classes to define the events and states related to the login functionality.

---
+++ lib/presentation/blocs/login/login_bloc.dart
@@ -0,0 +1,47 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:movieapp/common/constants/translation_constants.dart';
+import 'package:movieapp/domain/entities/app_error.dart';
+import 'package:movieapp/domain/entities/login_request_params.dart';
+import 'package:movieapp/domain/usecases/login_user.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final LoginUser loginUser;
+
+  LoginBloc({required this.loginUser}) : super(LoginInitial()) {
+    on<LoginEvent>(
+      (event, emit) async {
+        if (event is LoginInitiateEvent) {
+          final Either<AppError, bool> eitherResponse = await loginUser(
+            LoginRequestParams(
+                userName: event.username, password: event.password),
+          );
+          eitherResponse.fold(
+            (l) {
+              var message = getErrorMessage(l.appErrorType);
+              return LoginError(message);
+            },
+            (r) => emit(LoginSuccess()),
+          );
+        }
+      },
+    );
+  }
+  String getErrorMessage(AppErrorType appErrorType) {
+    switch (appErrorType) {
+      case AppErrorType.network:
+        return TranslationConstants.noNetwork;
+      case AppErrorType.api:
+      case AppErrorType.database:
+        return TranslationConstants.somethingWentWrong;
+      case AppErrorType.sessionDenied:
+        return TranslationConstants.sessionDenied;
+      default:
+        return TranslationConstants.wrongUsernamePassword;
+    }
+  }
+}
---
+++ lib/presentation/blocs/login/login_event.dart
@@ -0,0 +1,19 @@
+part of 'login_bloc.dart';
+
+sealed class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoginInitiateEvent extends LoginEvent {
+  final String username, password;
+
+  const LoginInitiateEvent({required this.username, required this.password});
+
+  @override
+  List<Object> get props => [username, password];
+}
+
+class LogoutEvent extends LoginEvent {}
