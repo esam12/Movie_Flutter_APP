261b7e7e07b8c21d3de8a15d47e8c9d2d2a676ba
ISAM ELZOBI <mobarmeg.9@gmail.com>
2024-11-20 17:13:30 +0300

    Add LoadingBloc and related event/state classes
    Introduce a new `LoadingBloc` to manage loading states in the application. This includes the `LoadingEvent` class with `StartLoading` and `StopLoading` events, and the `LoadingState` class with `LoadingInitial`, `LoadingStarted`, and `LoadingStopped` states.

---
+++ lib/presentation/blocs/loading/loading_state.dart
@@ -0,0 +1,14 @@
+part of 'loading_bloc.dart';
+
+sealed class LoadingState extends Equatable {
+  const LoadingState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class LoadingInitial extends LoadingState {}
+
+final class LoadingStarted extends LoadingState {}
+
+final class LoadingStopped extends LoadingState {}
